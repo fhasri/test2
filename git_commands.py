b28f3a31bc6289c49ebe85c0d9adaa4e34722aed
Bektur <bekeshjr@gmail.com>
2023-03-01 17:40:03 +0600

    files were added to .git

---
+++ git_commands.py
@@ -0,0 +1,14 @@
+  git  init ->  prevrashaet tekushyi  derectory v reposetoryi ( sosdaetsya .git) 
+  git add .(.  -> ALL files, or we  can type a name of the file ) otslejivanie failov 
+  git commit -m "commit"  -> dabavlyat / sovershaet sohronenie v git korotye nahoditsya v indexe 
+  git log  -> vyvodit jurnal ili (log) sograneniy 
+  git status  -> pokazyvaet status failov 
+  git remote add origin(nozvanie svyazi ) ssh( link or in russian sylka na udalennyi repazitoryi)
+  git checkout  -> hash_commit -> perekluchalis k drugoi versii koda ili perekluchalis. 
+  git branch -> spisok vseh vetok (zvezdychkoi pomechena aktivnaya vetka *) 
+  git checkout <nozvanie vetki>  
+                                       perehod na ukazonnuiu vetku 
+  git switch <nazvanie vetki>  
+  git puch origin(nazvanie_svyazi)  master(nazvanie_vetki)  -> otpravka versii koda na udalennyi repazitoriy 
+
+  git clone  <ssh link from where we want to copy / clone the code>  copy repository / clone 
